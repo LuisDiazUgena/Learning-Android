505a415b11b7409c7a567bb76f993a03a4d2a485
Luis Diaz <luisdiazugena@gmail.com>
2015-07-22 17:54:00 +0200

    keep working on it

--- simple_BT/app/src/main/java/com/luisdiaz/simple_bt/MainActivity.java
+++ simple_BT/app/src/main/java/com/luisdiaz/simple_bt/MainActivity.java
@@ -39,13 +39,64 @@ public class MainActivity extends ActionBarActivity {
     private BluetoothSocket btSocket = null;
     private OutputStream outStream = null;
 
+    //UUID V4 random generated
+    private static final UUID MY_UUID = UUID.fromString("78514d86-0c1a-4bb0-a68f-0d7ae849eda1");
+
+    //Mac Address (hardcoded)
+    private static String address = "00:00:00:00:00:00";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         outputText = (TextView) findViewById(R.id.OutputText);
+        AddText("In on Create");
+        //outputText.append("\nIn on create.");
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        CheckBTState();
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        AddText("In On Start");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AddText("in On Resume");
+        AddText("Trying to connect to BT device");
+
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try{
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        }catch (IOException e){
+            AlertBox("Fatal Error","In onResume(). Socket create failed:" +e.getMessage()+".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        //Establish connection
+        try{
+            btSocket.connect();
+        }catch (IOException e){
+            try{
+                btSocket.close();
+            }catch (IOException e2){
+                AlertBox("Fatal Error","In onResume. Unable to close socket during connection failure: "+e2.getMessage()+".");
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,5 +118,34 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void CheckBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+        // Emulator doesn't support Bluetooth and will return null
+        if(btAdapter==null) {
+            AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+                out.append("\n...Bluetooth is enabled...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+    public void AlertBox( String title, String message ){
+        new AlertDialog.Builder(this)
+                .setTitle( title )
+                .setMessage( message + " Press OK to exit." )
+                .setPositiveButton("OK", new OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        finish();
+                    }
+                }).show();
+    }
+    public void AddText(String text){
+        outputText.append("\n"+text+"\n");
     }
 }
