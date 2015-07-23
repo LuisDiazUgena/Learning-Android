8b1f89b60cebf7d9b9c10d8af3e806e112fce95e
Luis Diaz <luisdiazugena@gmail.com>
2015-07-23 17:06:26 +0200

    Not working. Stuck there so moving on.

---
+++ Spinner/app/src/main/java/com/luisdiaz/spinner/MainActivity.java
@@ -0,0 +1,65 @@
+package com.luisdiaz.spinner;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    final String[] datos = {"Elem1","Elem2","Elem3","Elem4","Elem5"};
+    private Spinner cmbOpciones;
+    private TextView lblMensaje;
+
+    ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,datos);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cmbOpciones = (Spinner) findViewById(R.id.CmbOpciones);
+        lblMensaje = (TextView) findViewById(R.id.lblMensaje);
+
+        adaptador.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cmbOpciones.setAdapter(adaptador);
+
+        cmbOpciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                lblMensaje.setText("Seleccionado " +parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                lblMensaje.setText("Nada seleccionado");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
