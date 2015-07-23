ae92114a3d3a180ac4932f1c008c07d2baa28b2e
Luis Diaz <luisdiazugena@gmail.com>
2015-07-23 12:26:15 +0200

    Keep Working on it

--- simple_BT/app/src/main/java/com/luisdiaz/simple_bt/MainActivity.java
+++ simple_BT/app/src/main/java/com/luisdiaz/simple_bt/MainActivity.java
@@ -3,8 +3,10 @@
 package com.luisdiaz.simple_bt;
 
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.support.v7.app.*;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -14,8 +16,11 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 
 //UI Elements
+import android.view.View;
 import android.widget.Toast;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 //Others
 import android.content.Intent;
@@ -28,10 +33,16 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    //Debug variables
+    private static final String TAG = "simpleBT";
+    private static final boolean DEBUG = true;
 
     //UI Variables
     TextView outputText;
+    Button sendButton,connectButton,disconnectButton;
+    TextView sendText;
 
     //BT Variables
     private static final int REQUEST_ENABLE_BT = 1;
@@ -43,58 +54,88 @@ public class MainActivity extends ActionBarActivity {
     private static final UUID MY_UUID = UUID.fromString("78514d86-0c1a-4bb0-a68f-0d7ae849eda1");
 
     //Mac Address (hardcoded)
-    private static String address = "00:00:00:00:00:00";
+    private static String address = "90:E5:42:E0:1C:91";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Buttons
+        sendButton = (Button) findViewById(R.id.SendButton);
+        connectButton = (Button) findViewById(R.id.BtnConnect);
+        disconnectButton = (Button) findViewById(R.id.BtnDisconnect);
+
+        //Text
+        sendText = (EditText) findViewById(R.id.SendText);
         outputText = (TextView) findViewById(R.id.OutputText);
-        AddText("In on Create");
-        //outputText.append("\nIn on create.");
+
+        LogText("In on Create");
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
-        CheckBTState();
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        AddText("In On Start");
+        LogText("In On Start");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        AddText("in On Resume");
-        AddText("Trying to connect to BT device");
+        LogText("in On Resume");
 
-        BluetoothDevice device = btAdapter.getRemoteDevice(address);
-        // Two things are needed to make a connection:
-        //   A MAC address, which we got above.
-        //   A Service ID or UUID.  In this case we are using the
-        //     UUID for SPP.
-        try{
-            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
-        }catch (IOException e){
-            AlertBox("Fatal Error","In onResume(). Socket create failed:" +e.getMessage()+".");
-        }
-
-        // Discovery is resource intensive.  Make sure it isn't going on
-        // when you attempt to connect and pass your message.
-        btAdapter.cancelDiscovery();
-
-        //Establish connection
-        try{
-            btSocket.connect();
-        }catch (IOException e){
-            try{
-                btSocket.close();
-            }catch (IOException e2){
-                AlertBox("Fatal Error","In onResume. Unable to close socket during connection failure: "+e2.getMessage()+".");
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String message = sendText.getText().toString(); // Message to send is here!
+                outputText.setText(message);
             }
-        }
+        });
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CheckBTState();
+                LogText("Trying to connect to BT device");
+
+                BluetoothDevice device = btAdapter.getRemoteDevice(address);
+                // Two things are needed to make a connection:
+                //   A MAC address, which we got above.
+                //   A Service ID or UUID.  In this case we are using the
+                //     UUID for SPP.
+                try{
+                    btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+                }catch (IOException e){
+                    AlertBox("Fatal Error","In onResume(). Socket create failed:" +e.getMessage()+".");
+                }
+
+                // Discovery is resource intensive.  Make sure it isn't going on
+                // when you attempt to connect and pass your message.
+                btAdapter.cancelDiscovery();
+
+                //Establish connection
+                try{
+                    btSocket.connect();
+                }catch (IOException e){
+                    try{
+                        btSocket.close();
+                    }catch (IOException e2){
+                        AlertBox("Fatal Error","In onResume. Unable to close socket during connection failure: "+e2.getMessage()+".");
+                    }
+                }
+
+                //create a data stream to talk to server
+                try{
+                    outStream = btSocket.getOutputStream();
+                }catch (IOException e){
+                    AlertBox("Fatal Error","In onResume. Unable to close socket during connection failure: "+e.getMessage()+".");
+                }
+            }
+        });
     }
 
 
@@ -127,7 +168,7 @@ public class MainActivity extends ActionBarActivity {
             AlertBox("Fatal Error", "Bluetooth Not supported. Aborting.");
         } else {
             if (btAdapter.isEnabled()) {
-                out.append("\n...Bluetooth is enabled...");
+                LogText("\n...Bluetooth is enabled...");
             } else {
                 //Prompt user to turn on Bluetooth
                 Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
@@ -145,7 +186,10 @@ public class MainActivity extends ActionBarActivity {
                     }
                 }).show();
     }
-    public void AddText(String text){
-        outputText.append("\n"+text+"\n");
+    public void LogText(String text){
+        if (DEBUG){
+            Log.e(TAG,text);
+        }
+        //outputText.append("\n"+text+"\n");
     }
 }
