0df840d99786e56d539c7dabc4d6b8cb0868b9b5
Luis Diaz <luisdiazugena@gmail.com>
2015-08-11 13:10:44 +0200

    working on a custom view

--- GPSSpeedWarning/app/src/main/java/com/luisdiaz/gpsspeedwarning/ArcSpeed.java
+++ GPSSpeedWarning/app/src/main/java/com/luisdiaz/gpsspeedwarning/ArcSpeed.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 
@@ -15,10 +16,13 @@ import android.view.WindowManager;
 /**
  * Created by luis-diaz on 11/08/15.
  */
+
 public class ArcSpeed extends View{
 
     public float w,h;
     private int margin = 10;
+    private Paint p;
+    private RectF rectF;
 
     public ArcSpeed(Context context, AttributeSet attrs){
         super(context, attrs);
@@ -30,19 +34,22 @@ public class ArcSpeed extends View{
 
         w = width;
         h = height/2;
+
+        p = new Paint();
+        //x,y,width, height
+        rectF = new RectF(0, 0,w, w);
     }
 
     @Override
     protected void onDraw(Canvas canvas){
-        Paint p = new Paint();
-        //x,y,ancho, alto
-        RectF rectF = new RectF(0, 0,w, w);
+
         p.setColor(Color.BLACK);
-        canvas.drawArc (rectF, 0, 200, true, p);
+        canvas.drawArc (rectF, 0, -90, true, p);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldW,int oldH){
+
     }
 
 
